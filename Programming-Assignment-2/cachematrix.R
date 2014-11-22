eeda2b20aa976d25ba5b88bb0f573e405b32343a
Rafal <szczurasek@poczta.onet.pl>
2014-11-22 22:47:12 +0100

    cachematrix

--- Programming-Assignment-2/cachematrix.R
+++ Programming-Assignment-2/cachematrix.R
@@ -3,7 +3,43 @@
 
 ## Write a short comment describing this function
 
+## #############################################################################
+## Creates a matrix object with cachable inverse
+##
+##  x is a formal parameter (source matrix)
+##
+##  inv is the variable in which the inverse will be saved
+##
+##  set and get                  are methods that allow the stored matrix                         to be saved or retrieved
+##  set.Inverse and get.Inverse  are methods that allow the stored inverse 
+##                               of the matrix  to be saved or retrieved
+## =============================================================================
+
 makeCacheMatrix <- function(x = matrix()) {
+
+  ## Initialize the inverse
+  inv <- NULL                       
+
+  ## Method to save the stored marix
+  set <- function(matrix) {            
+    x <<- matrix
+    inv <<- NULL
+  }
+  
+  ## method to retrieve the stored marix
+  get <- function() x                     
+
+  ## Method to save the stored inverse of the matrix 
+  set.Inverse <- function(inverse) inv <<- inverse  
+
+  ## Method to retrieve the stored inverse of the matrix
+  get.Inverse <- function() inv                     
+
+  ## Return a list of the methods
+  list(set = set, get = get,
+       set.Inverse = set.Inverse,
+       get.Inverse = get.Inverse
+  )
 
 }
 
